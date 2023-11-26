00cb65e3bb2671b43990defcc5cf3f9887eced56
Nazar Vinnichuk <nazar@vinnich.uk>
2023-11-26 19:49:43 +0200

    Improve the fourth lab.

--- lab-4/main.py
+++ lab-4/main.py
@@ -1,45 +1,65 @@
 from pprint import pprint
 
 teachers = {
-    "greg": [7, ["math", "programming"]],
-    "steve": [7, ["programming", "physics"]],
-    "rhod": [7, ["physics", "math"]],
-    "tom": [3, ["linguistics"]],
+    "greg": [12, ["math", "programming"]],
+    "steve": [12, ["programming", "physics"]],
+    "rhod": [8, ["physics", "math"]],
+    "sean": [15, ["physics", "calculus", "programming"]],
+    "tom": [4, ["linguistics"]],
+    "neil": [9, ["physics"]],
+    "rick": [10, ["calculus"]],
+    "phil": [10, ["linguistics", "math", "calculus"]],
 }
 
 groups = {
     "mathematicians": {
-        "math": 6,
-        "physics": 1,
-        "linguistics": 1,
+        "math": 8,
+        "calculus": 6,
+        "physics": 4,
+        "linguistics": 2,
     },
     "programmers": {
         "math": 2,
-        "programming": 4,
-        "physics": 1,
-        "linguistics": 1,
+        "calculus": 2,
+        "programming": 12,
+        "physics": 2,
+        "linguistics": 2,
     },
     "physicists": {
-        "math": 2,
-        "programming": 1,
-        "physics": 4,
-        "linguistics": 1,
+        "math": 1,
+        "calculus": 5,
+        "programming": 4,
+        "physics": 10,
+    },
+    "data-scientists": {
+        "math": 7,
+        "calculus": 2,
+        "programming": 7,
+        "physics": 1,
+        "linguistics": 3,
     },
 }
 
-schedule = {group: [None for _ in range(10)] for group in groups}
+schedule = {group: [None for _ in range(20)] for group in groups}
 
 
 def get_teachers(subject, hour):
-    return [
-        teacher
-        for teacher in teachers
-        if teachers[teacher][0]
-        and subject in teachers[teacher][1]
-        and not any(
-            hours[hour] and hours[hour][1] == teacher for hours in schedule.values()
-        )
-    ]
+    return sorted(
+        (
+            teacher
+            for teacher in teachers
+            if teachers[teacher][0]
+            and subject in teachers[teacher][1]
+            and not any(
+                hours[hour] and hours[hour][1] == teacher for hours in schedule.values()
+            )
+        ),
+        key=teacher_sorting_key,
+    )
+
+
+def teacher_sorting_key(teacher):
+    return len(teachers[teacher][1])
 
 
 subject_demand = {}
@@ -56,6 +76,13 @@ for teacher in teachers:
         if subject not in subject_supply:
             subject_supply[subject] = 0
         subject_supply[subject] += teachers[teacher][0]
+
+
+def propagate_constraints(subject, teacher, delta):
+    subject_demand[subject] -= delta
+
+    for subject in teachers[teacher][1]:
+        subject_supply[subject] -= delta
 
 
 def forward_check():
@@ -79,10 +106,7 @@ def solve():
                     schedule[group][hour] = (subject, teacher)
                     groups[group][subject] -= 1
                     teachers[teacher][0] -= 1
-                    subject_demand[subject] -= 1
-
-                    for subject in teachers[teacher][1]:
-                        subject_supply[subject] -= 1
+                    propagate_constraints(subject, teacher, 1)
 
                     if forward_check():
                         pprint(schedule)
@@ -93,10 +117,7 @@ def solve():
                     schedule[group][hour] = None
                     groups[group][subject] += 1
                     teachers[teacher][0] += 1
-                    subject_demand[subject] += 1
-
-                    for subject in teachers[teacher][1]:
-                        subject_supply[subject] += 1
+                    propagate_constraints(subject, teacher, -1)
 
     return not any(any(groups[group].values()) for group in groups)
 
